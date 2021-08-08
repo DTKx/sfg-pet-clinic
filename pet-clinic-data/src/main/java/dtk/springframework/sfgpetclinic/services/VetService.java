9e8abfe0204a63967d5f40c020d96de0440d26db
Debora <dkx1003@ubu.alu.es>
2021-08-08 08:58:59 -0300

    Refactors to common crud interface. closes #11

--- pet-clinic-data/src/main/java/dtk/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/dtk/springframework/sfgpetclinic/services/VetService.java
@@ -1,11 +1,9 @@
 package dtk.springframework.sfgpetclinic.services;
 
 import dtk.springframework.sfgpetclinic.model.Pet;
+import dtk.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
 }
